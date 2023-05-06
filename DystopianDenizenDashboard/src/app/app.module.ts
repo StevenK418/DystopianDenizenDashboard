285400047bb1bbd5f06b213b869886227f5c7aa6
StevenK418 <stevenkelly418@gmail.com>
2023-05-06 17:08:09 +0100

    Broke header, footer and sidebar drawer into separate components.

--- DystopianDenizenDashboard/src/app/app.module.ts
+++ DystopianDenizenDashboard/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DrawerComponent } from './drawer/drawer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawerComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
