a8fcd3f0c32615c534e5c5cc47b5f5e6adf82365
StevenK418 <stevenkelly418@gmail.com>
2023-05-06 09:45:16 +0100

    Removing ng-bootstrap module with dependency conflicts.

--- DystopianDenizenDashboard/src/app/app.module.ts
+++ DystopianDenizenDashboard/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -12,7 +11,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
     AppRoutingModule
   ],
   providers: [],
