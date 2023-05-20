73b92f33f7c7eed1ba4605ef2a52a5beb3dbde1e
StevenK418 <stevenkelly418@gmail.com>
2023-05-20 13:33:38 +0100

    Added new about page design and layout. Fixed issue with dynamically driven carousel displaying all images at once. Tweaked structure of blogPost model.

--- DystopianDenizenDashboard/src/app/models/blogpost.ts
+++ DystopianDenizenDashboard/src/app/models/blogpost.ts
@@ -1,7 +1,9 @@
 export interface Blogpost
 {
-    _id: string,
-    title: string,
-    body: string,
+    _id: string;
+    title: string;
+    body: string;
+    author: string;
+    dateCreated: string;
     featuredImage: string;
 }
